8790f0fd42e0e251e21d90b36806416e992b7343
Manuja DeSilva <mdesilva@bu.edu>
2020-02-27 16:46:59 -0500

    linker issue

---
+++ initDB.cpp
@@ -0,0 +1,10 @@
+#include "rocksdb/db.h"
+
+using namespace std;
+
+int main() {
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/testdb", &db);
+}
